21bd5754193d269ddb74fa60b924b8cd551d21b9
Yyuki78 <yuki.yamada2411@gmail.com>
2022-04-28 18:19:46 +0900

    講義内で使用した部分＋VisualStadioが開いているとエラーが出るの注意

---
+++ PUN2-Eracer/Assets/Scripts/Test/GamePlayer.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class GamePlayer : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Projectile projectilePrefab = default;
+
+    private ProjectileManager projectileManager;
+
+    private void Awake()
+    {
+        projectileManager = GameObject.FindWithTag("ProjectileManager").GetComponent<ProjectileManager>();
+    }
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+            var dv = 6f * Time.deltaTime * direction;
+            transform.Translate(dv.x, dv.y, 0f);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                var playerWorldPosition = transform.position;
+                var mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                var dp = mouseWorldPosition - playerWorldPosition;
+                float angle = Mathf.Atan2(dp.y, dp.x);
+
+                // FireProjectile(angle)をRPCで実行する
+                photonView.RPC(nameof(FireProjectile), RpcTarget.All, angle);
+            }
+        }
+    }
+
+    // [PunRPC]属性をつけると、RPCでの実行が有効になる
+    [PunRPC]
+    private void FireProjectile(float angle)
+    {
+        projectileManager.Fire(transform.position, angle);
+    }
+}
---
+++ PUN2-Eracer/Assets/Scripts/Test/SampleScene.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+// MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする
+public class SampleScene : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+
+        PhotonNetwork.SendRate = 20; // 1秒間にメッセージ送信を行う回数
+        PhotonNetwork.SerializationRate = 10; // 1秒間にオブジェクト同期を行う回数
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster()
+    {
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // マッチングが成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom()
+    {
+        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
+        var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("GamePlayer", v, Quaternion.identity);
+    }
+}
